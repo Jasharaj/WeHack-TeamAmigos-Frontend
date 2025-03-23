9f7fc30180b71142c7f63e57a0bbddcf8c859abc
Jasharaj <jasharaj38624@outlook.com>
2025-03-23 14:07:44 +0530

    Update BASE_URL

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@
 
 const config = {
   // Base URL for the backend API
-  BASE_URL: 'http://localhost:5000',
+  // BASE_URL: 'http://localhost:5000',
+  BASE_URL: 'https://wehack-fkon.onrender.com',
 };
 
 export default config;
