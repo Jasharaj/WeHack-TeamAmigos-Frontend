26fed6211b7115a0d4717ebbdf21e53a12aefb4f
Jasharaj <jasharaj38624@outlook.com>
2025-03-23 13:49:53 +0530

    Empty repository

--- src/app/register/page.tsx
+++
@@ -1,5 +0,0 @@
-import RegistrationPage from '@/components/RegistrationPage';
-
-export default function Register() {
-  return <RegistrationPage />;
-}
--- src/app/user-dashboard/layout.tsx
+++
@@ -1,5 +0,0 @@
-import UserDashboardLayout from '@/components/user-dashboard/UserDashboardLayout';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <UserDashboardLayout>{children}</UserDashboardLayout>;
-}
